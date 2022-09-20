c4ffeae802003994b2c963301cb2da64c9305906
Parsa <mohammadkazemi.parsa@gmail.com>
2022-09-20 10:52:00 +0430

    beautify files

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let hourtxt = document.querySelector(".hourtxt h1");
 let minutetxt = document.querySelector(".minutetxt h1");
 let secondtxt = document.querySelector(".secondtxt h1");
 
-function SetTime(){
+function SetTime() {
     let date = new Date();
     let Hour = date.getHours();
     let Minute = date.getMinutes();
@@ -13,19 +13,19 @@ function SetTime(){
     minutetxt.textContent = Minute;
 
     // check if the varibales are less than 10 if they were so add "0" + the variable
-    if(Hour < 10){
+    if (Hour < 10) {
         hourtxt.textContent = "0" + Hour;
     }
-    if(Minute < 10){
+    if (Minute < 10) {
         minutetxt.textContent = "0" + Minute;
     }
-    if(Second < 10){
+    if (Second < 10) {
         secondtxt.textContent = "0" + Second;
     }
 
 
 }
 
-setInterval(()=>{
+setInterval(() => {
     SetTime();
-},1000)
+}, 1000)
